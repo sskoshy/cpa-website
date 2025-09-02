e33da6e00a55fa86cdea63ccdc60823eaed76640
Sara Koshy <sskoshy@ucdavis.edu>
2025-09-02 12:09:02 -0700

    Deployable backend + CORS + /api

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://sskoshy.github.io"],
     credentials: true,
   })
 );
